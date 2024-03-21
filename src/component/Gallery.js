accccc5b5489beb9176c5e55a7f7d59d22bee650
Bae SeungHee <66785642+b-sseung@users.noreply.github.com>
2024-03-21 19:21:55 +0900

    240321

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import styled, {css} from "styled-components";
 import { BiTable, BiCalendar } from "react-icons/bi";
+import $ from 'jquery';
+
+const uploadFileForm = (state, action) => {
+    state = {...state, [action.type]: action.value};
+    console.log(state);
+    return state;
+}
 
 const Gallery = () => {
     const [mode, setMode] = useState('calendar');
-    const CheckBox = ({value, text}) => {
+    const [commonChk, setCommonChk] = useState(false);
+    const [joowanChk, setJoowanChk] = useState(false);
+    const [taebinChk, setTaebinChk] = useState(false);
+
+    const CheckBox = ({value, text, checked, onClick}) => {
         return (
             <>
-                <input type="checkbox" value={value} id={value}></input>
+                <input type="checkbox" value={value} id={value} onChange={onClick} checked={checked}></input>
                 <label htmlFor={value}>{text}</label>
             </>
         );
@@ -18,12 +29,44 @@ const Gallery = () => {
         padding: '5px'
     }
 
+    const targetClick = (e) => {
+        switch (e.target.value) {
+            case 'common' :
+                setCommonChk(!commonChk);
+                if (!commonChk) {
+                    setJoowanChk(true);
+                    setTaebinChk(true);
+                } else {
+                    setJoowanChk(false);
+                    setTaebinChk(false);
+                }
+                
+                break;
+            case 'joowan': 
+                setJoowanChk(!joowanChk);
+                if (!joowanChk && taebinChk) {
+                    setCommonChk(true);
+                } else {
+                    setCommonChk(false);
+                }
+                break;
+            case 'taebin':
+                setTaebinChk(!taebinChk);
+                if (!taebinChk && joowanChk) {
+                    setCommonChk(true);
+                } else {
+                    setCommonChk(false);
+                }
+                break;
+        }
+    }
+
     return (
-        <>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
             <div>
-                <CheckBox value="common" text="전체"></CheckBox>
-                <CheckBox value="joowan" text="차주완"></CheckBox>
-                <CheckBox value="taebin" text="이태빈"></CheckBox>
+                <CheckBox value="common" text="전체" checked={commonChk} onClick={targetClick}></CheckBox>
+                <CheckBox value="joowan" text="차주완" checked={joowanChk} onClick={targetClick}></CheckBox>
+                <CheckBox value="taebin" text="이태빈" checked={taebinChk} onClick={targetClick}></CheckBox>
             </div>
             <div>
                 <p>상세검색</p>
@@ -32,7 +75,7 @@ const Gallery = () => {
                 <BiCalendar style={iconStyle} enableBackground="black" size='25' color={mode === 'calendar' ? 'black' : 'lightgray'} onClick={() => setMode('calendar')}></BiCalendar>
                 <BiTable style={iconStyle} size='25' color={mode === 'table' ? 'black' : 'lightgray'} onClick={() => setMode('table')}></BiTable>
             </div>
-        </>
+        </ div>
     )
     
 }
